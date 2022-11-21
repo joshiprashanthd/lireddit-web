cbd9c51da825824c29134b19cfda5e3a492849d4
Prashant Joshi <joshiprashanthd121@gmail.com>
2022-11-21 23:02:35 +0530

    modify ui

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -11,38 +11,37 @@ import { createUrqlClient } from '../utils/createUrqlClient'
 import { useIsAuth } from '../utils/useIsAuth'
 
 const CreatePost: React.FC<{}> = ({}) => {
-    const router = useRouter()
-    useIsAuth()
-    const [, createPost] = useCreatePostMutation()
-    return (
-        <Layout variant="small">
-            <Heading mb={8}>Create Post</Heading>
-            <Formik
-                initialValues={{ title: '', text: '' }}
-                onSubmit={async (values) => {
-                    const { error } = await createPost({ input: values })
-                    console.log(error?.message)
-                    if (!error) {
-                        router.push('/')
-                    }
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <InputField name="title" label="Title" />
-                        <Box mt={4}>
-                            <InputField name="text" label="Body" textarea />
-                        </Box>
-                        <Box mt={4}>
-                            <Button type="submit" isLoading={isSubmitting}>
-                                Post
-                            </Button>
-                        </Box>
-                    </Form>
-                )}
-            </Formik>
-        </Layout>
-    )
+  const router = useRouter()
+  useIsAuth()
+  const [, createPost] = useCreatePostMutation()
+  return (
+    <Layout variant="small">
+      <Heading mb={8}>Create Post</Heading>
+      <Formik
+        initialValues={{ title: '', text: '' }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values })
+          if (!error) {
+            router.push('/')
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" label="Title" />
+            <Box mt={4}>
+              <InputField name="text" label="Body" textarea />
+            </Box>
+            <Box mt={4}>
+              <Button type="submit" isLoading={isSubmitting}>
+                Post
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  )
 }
 
 export default withUrqlClient(createUrqlClient)(CreatePost)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,42 @@
-import { Button } from '@chakra-ui/react'
+import { Box, Button, Center, Heading, Stack, Text } from '@chakra-ui/react'
 import { withUrqlClient } from 'next-urql'
 import { Layout } from '../components/Layout'
 import { usePostsQuery } from '../gql/graphql'
 import { createUrqlClient } from '../utils/createUrqlClient'
-import NextLink from 'next/link'
 
 const Index = () => {
-    const [{ data }] = usePostsQuery()
-    return (
-        <Layout>
-            <NextLink href="/create-post">
-                <Button>Create Post</Button>
-            </NextLink>
-            {data ? (
-                data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-            ) : (
-                <div>Loading...</div>
-            )}
-        </Layout>
-    )
+  const [{ data, fetching }] = usePostsQuery({
+    variables: {
+      limit: 5,
+    },
+  })
+  if (!data && !fetching) return <Box>You got query failed for some reason</Box>
+
+  return (
+    <Layout>
+      <Stack spacing={8}>
+        {!data && fetching ? (
+          <div>Loading...</div>
+        ) : (
+          data!.posts.map((p) => {
+            return (
+              <Box key={p.id} p={4} borderRadius="lg" boxShadow="lg">
+                <Heading fontSize="lg">{p.title}</Heading>
+                <Text mt={4}>{p.textSnippet}...</Text>
+              </Box>
+            )
+          })
+        )}
+      </Stack>
+      {data && (
+        <Center>
+          <Button my={8}>Load More</Button>
+        </Center>
+      )}
+    </Layout>
+  )
 }
 
 export default withUrqlClient(createUrqlClient, {
-    ssr: true,
+  ssr: true,
 })(Index)

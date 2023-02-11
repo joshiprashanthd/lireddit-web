65bc53fed3f4ff390b613b3ef675cf6efa612e3b
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-02-11 21:56:07 +0530

    add `load more` logic to index page and use `useIsServer` hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import { Box, Button, Center, Heading, Stack, Text } from '@chakra-ui/react'
+import { getDataFromTree } from '@apollo/client/react/ssr'
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 import { Layout } from '../components/Layout'
+import { VoteSection } from '../components/VoteSection'
 import { usePostsQuery } from '../gql/graphql'
 import withApollo from '../utils/withApollo'
 
 const Index = () => {
-  const { data, loading } = usePostsQuery({
+  const { data, loading, fetchMore } = usePostsQuery({
     variables: {
       limit: 5,
     },
@@ -14,26 +25,43 @@ const Index = () => {
   return (
     <Layout>
       <Stack spacing={8}>
-        {!data && loading ? (
-          <div>Loading...</div>
-        ) : (
-          data!.posts.map((p) => {
+        {data &&
+          data!.posts.posts.map((p) => {
             return (
               <Box key={p.id} p={4} borderRadius="lg" boxShadow="lg">
-                <Heading fontSize="lg">{p.title}</Heading>
-                <Text mt={4}>{p.textSnippet}...</Text>
+                <HStack align="start">
+                  <Box mr="4">
+                    <VoteSection post={p} />
+                  </Box>
+                  <VStack align="start">
+                    <Heading fontSize="lg">{p.title}</Heading>
+                    <Text>posted by {p.creator.username}</Text>
+                    <Text mt={4}>{p.textSnippet}...</Text>
+                  </VStack>
+                </HStack>
               </Box>
             )
-          })
-        )}
+          })}
       </Stack>
-      {data && (
+      {data && data.posts.hasMore && (
         <Center>
-          <Button my={8}>Load More</Button>
+          <Button
+            my={8}
+            onClick={() =>
+              fetchMore({
+                variables: {
+                  cursor:
+                    data.posts.posts[data.posts.posts.length - 1].createdAt,
+                },
+              })
+            }
+          >
+            Load More
+          </Button>
         </Center>
       )}
     </Layout>
   )
 }
 
-export default withApollo({ ssr: true })(Index)
+export default withApollo(Index, { getDataFromTree })

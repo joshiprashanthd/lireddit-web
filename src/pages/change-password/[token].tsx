bacf82665f277ddb60840a216a8cc2b28e4d026a
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-02-14 12:18:15 +0530

    use core components for layout and buttons

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,12 +1,13 @@
-import { Button, Flex, Heading, Link, useToast } from '@chakra-ui/react'
+import { Flex, Heading, Link, useToast } from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
-import { NextPage } from 'next'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { RButton } from '../../components/core/RButton'
+import { RCard } from '../../components/core/RCard'
 
 import InputField from '../../components/InputField'
-import { Wrapper } from '../../components/Wrapper'
+import { Layout } from '../../components/Layout'
 import { useChangePasswordMutation } from '../../gql/graphql'
 import { toErrorMap } from '../../utils/toErrorMap'
 import withApollo from '../../utils/withApollo'
@@ -30,53 +31,55 @@ const ChangePassword = () => {
   }, [tokenError])
 
   return (
-    <Wrapper variant="small">
-      <Heading mb={8}>Change Password</Heading>
-      <Formik
-        initialValues={{ newPassword: '' }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await changePassword({
-            variables: {
-              ...values,
-              token:
-                typeof router.query.token === 'string'
-                  ? router.query.token
-                  : '',
-            },
-          })
-          if (response.data?.changePassword.errors) {
-            const errorMap = toErrorMap(response.data.changePassword.errors)
+    <Layout variant="small">
+      <RCard>
+        <Heading mb={8}>Change Password</Heading>
+        <Formik
+          initialValues={{ newPassword: '' }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await changePassword({
+              variables: {
+                ...values,
+                token:
+                  typeof router.query.token === 'string'
+                    ? router.query.token
+                    : '',
+              },
+            })
+            if (response.data?.changePassword.errors) {
+              const errorMap = toErrorMap(response.data.changePassword.errors)
 
-            if ('token' in errorMap) {
-              setTokenError(errorMap['token'])
-            } else {
-              setErrors(errorMap)
-            }
-          } else if (response.data?.changePassword.user) router.push('/')
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="newPassword"
-              label="New Password"
-              type="password"
-            />
-            <Flex align="center" justify="space-between" mt={4}>
-              <Button type="submit" isLoading={isSubmitting}>
-                Update
-              </Button>
-              {tokenError != null ? (
-                <NextLink href="/forgot-password">
-                  <Link color="red.600">Retry Forgot Password</Link>
-                </NextLink>
-              ) : null}
-            </Flex>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+              if ('token' in errorMap) {
+                setTokenError(errorMap['token'])
+              } else {
+                setErrors(errorMap)
+              }
+            } else if (response.data?.changePassword.user) router.push('/')
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name="newPassword"
+                label="New Password"
+                type="password"
+              />
+              <Flex align="center" justify="space-between" mt={4}>
+                <RButton type="submit" isLoading={isSubmitting}>
+                  Update
+                </RButton>
+                {tokenError != null ? (
+                  <NextLink href="/forgot-password">
+                    <Link color="red.600">Retry Forgot Password</Link>
+                  </NextLink>
+                ) : null}
+              </Flex>
+            </Form>
+          )}
+        </Formik>
+      </RCard>
+    </Layout>
   )
 }
 
-export default withApollo()(ChangePassword)
+export default withApollo(ChangePassword)

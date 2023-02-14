5bf68a0e7248ece212e9f303fa6df80d35d20211
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-02-14 12:18:58 +0530

    use core components

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
+import { RButton } from '../components/core/RButton'
+import { RCard } from '../components/core/RCard'
 
 import InputField from '../components/InputField'
 import { Layout } from '../components/Layout'
@@ -22,47 +24,41 @@ const CreatePost = () => {
     ],
   })
   return (
-    <Layout variant="small">
-      <Heading mb={8} color="white">
-        Create Post
-      </Heading>
-      <Formik
-        initialValues={{ title: '', text: '' }}
-        onSubmit={async (values) => {
-          const response = await createPost({ variables: { input: values } })
-          if (!response.errors) {
-            router.push('/')
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="title"
-              label="Title"
-              borderColor="gray.700"
-              borderWidth="2px"
-            />
-            <Box mt={4}>
-              <InputField name="text" label="Body" textarea />
-            </Box>
-            <Button
-              minW="lg"
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              bg="purple.500"
-              color="white"
-              _hover={{
-                bg: 'purple.700',
-              }}
-              my={8}
-            >
-              Post
-            </Button>
-          </Form>
-        )}
-      </Formik>
+    <Layout variant="regular">
+      <RCard>
+        <Heading mb={8}>Create Post</Heading>
+        <Formik
+          initialValues={{ title: '', text: '' }}
+          onSubmit={async (values) => {
+            const response = await createPost({ variables: { input: values } })
+            if (!response.errors) {
+              router.push('/')
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name="title"
+                label="Title"
+                borderColor="gray.700"
+                borderWidth="2px"
+              />
+              <Box mt={4}>
+                <InputField name="text" label="Body" textarea />
+              </Box>
+              <RButton
+                minW="full"
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+              >
+                Post
+              </RButton>
+            </Form>
+          )}
+        </Formik>
+      </RCard>
     </Layout>
   )
 }
--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, Heading, useToast } from '@chakra-ui/react'
+import { Box, Heading, useToast } from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
 import { useState } from 'react'
+import { RButton } from '../components/core/RButton'
+import { RCard } from '../components/core/RCard'
 import InputField from '../components/InputField'
-import { Wrapper } from '../components/Wrapper'
+import { Layout } from '../components/Layout'
 import { useForgotPasswordMutation } from '../gql/graphql'
 import withApollo from '../utils/withApollo'
 
@@ -12,51 +14,53 @@ export const ForgotPassword = () => {
   const [complete, setComplete] = useState(false)
 
   return (
-    <Wrapper variant="small">
-      <Heading mb={8}>Forgot Password</Heading>
-      <Formik
-        initialValues={{ email: '' }}
-        onSubmit={async (values) => {
-          const response = await forgotPassword({ variables: values })
-          if (!response.data?.forgotPassword)
-            toast({
-              title: 'Something Went Wrong!!',
-              description: 'Make sure your email is correct',
-              status: 'error',
-              duration: 5000,
-              variant: 'left-accent',
-              isClosable: true,
-              position: 'bottom-right',
-            })
-          else if (response.data?.forgotPassword) {
-            toast({
-              title: 'Verification Email Sent',
-              description: 'Check your email to change password',
-              status: 'success',
-              variant: 'left-accent',
-              duration: 5000,
-              position: 'bottom-right',
-            })
-            setComplete(true)
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="email" label="Email" />
-            <Box mt={4}>
-              <Button
-                type="submit"
-                isLoading={isSubmitting}
-                disabled={complete}
-              >
-                Send Email
-              </Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+    <Layout variant="small">
+      <RCard>
+        <Heading mb={8}>Forgot Password</Heading>
+        <Formik
+          initialValues={{ email: '' }}
+          onSubmit={async (values) => {
+            const response = await forgotPassword({ variables: values })
+            if (!response.data?.forgotPassword)
+              toast({
+                title: 'Something Went Wrong!!',
+                description: 'Make sure your email is correct',
+                status: 'error',
+                duration: 5000,
+                variant: 'left-accent',
+                isClosable: true,
+                position: 'bottom-right',
+              })
+            else if (response.data?.forgotPassword) {
+              toast({
+                title: 'Verification Email Sent',
+                description: 'Check your email to change password',
+                status: 'success',
+                variant: 'left-accent',
+                duration: 5000,
+                position: 'bottom-right',
+              })
+              setComplete(true)
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField name="email" label="Email" />
+              <Box mt={4}>
+                <RButton
+                  type="submit"
+                  isLoading={isSubmitting}
+                  disabled={complete}
+                >
+                  Send Email
+                </RButton>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </RCard>
+    </Layout>
   )
 }
 

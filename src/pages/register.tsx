3d3ed5b1f96281c64fc410092eaf88fd9704bf4d
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-02-14 12:08:30 +0530

    use `Layout` and `RCard` component

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import { Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
+import { RButton } from '../components/core/RButton'
+import { RCard } from '../components/core/RCard'
 import InputField from '../components/InputField'
-import { Wrapper } from '../components/Wrapper'
+import { Layout } from '../components/Layout'
 import { useRegisterMutation } from '../gql/graphql'
 import { toErrorMap } from '../utils/toErrorMap'
 import withApollo from '../utils/withApollo'
@@ -23,35 +25,37 @@ const Register = () => {
   })
 
   return (
-    <Wrapper variant="small">
-      <Heading mb={8}>Register</Heading>
-      <Formik
-        initialValues={{ username: '', password: '', email: '' }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await register({ variables: { options: values } })
-          if (response.data?.register.errors)
-            setErrors(toErrorMap(response.data.register.errors))
-          else if (response.data?.register.user) router.push('/')
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="email" label="Email" type="email" />
-            <Box mt={4}>
-              <InputField name="username" label="Username" />
-            </Box>
-            <Box mt={4}>
-              <InputField name="password" label="Password" type="password" />
-            </Box>
-            <Box mt={4}>
-              <Button type="submit" isLoading={isSubmitting}>
-                Register
-              </Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+    <Layout variant="small">
+      <RCard>
+        <Heading mb={8}>Register</Heading>
+        <Formik
+          initialValues={{ username: '', password: '', email: '' }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register({ variables: { options: values } })
+            if (response.data?.register.errors)
+              setErrors(toErrorMap(response.data.register.errors))
+            else if (response.data?.register.user) router.push('/')
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField name="email" label="Email" type="email" />
+              <Box mt={4}>
+                <InputField name="username" label="Username" />
+              </Box>
+              <Box mt={4}>
+                <InputField name="password" label="Password" type="password" />
+              </Box>
+              <Box mt={4}>
+                <RButton type="submit" isLoading={isSubmitting} minW="full">
+                  Register
+                </RButton>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </RCard>
+    </Layout>
   )
 }
 

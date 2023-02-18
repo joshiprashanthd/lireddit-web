a495bdc729a868e697609278f505ddf48b153914
Prashant Joshi <joshiprashanthd121@gmail.com>
2023-02-18 13:45:27 +0530

    remove console.log

--- src/utils/useIsServer.ts
+++ src/utils/useIsServer.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 
 export const useIsServer = () => {
-  console.log('graphql server: ', process.env.GRAPHQL_SERVER)
   const [isServer, setIsServer] = useState(true)
 
   useEffect(() => {
